a8e1824f74f3a4ceb6de98304456559c067b23b1
guiandradedev <guiandradeemail@gmail.com>
2023-12-11 18:36:05 -0300

    Add Laravel Permission

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Define roles
+        $roles = ['user', 'commercial', 'producer', 'administrative'];
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
+
+        // // Permissions
+        // $permissionsWithRole = [
+        //     'permission' => [
+        //         'user',
+        //         'commercial'
+        //     ]
+        // ];
+
+        // foreach ($permissionsWithRole as $permission => $roles_permission) {
+        //     $createdPermission = Permission::create(['name' => $permission]);
+
+        //     foreach ($roles_permission as $roleName) {
+        //         $role = Role::findByName($roleName);
+        
+        //         if ($role) {
+        //             $role->givePermissionTo($createdPermission);
+        //         }
+        //     }
+        // }
+    }
+}
