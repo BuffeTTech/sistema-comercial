6f04fe51eedc048fbda46271b72a5f4c35f8c159
Guilherme Andrade <guiandradeemail@gmail.com>
2024-12-13 01:28:19 -0300

    fix: start new booking

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect(config('app.administrative_url'));
-})->name('landing_page');
+})->name('home');
 // Route::get('/{buffet}', [SiteController::class,'buffetTest'])->name('buffetTest');
 Route::get('/buffet', [SiteController::class,'buffetAlegria'])->name('buffetTest');
 
