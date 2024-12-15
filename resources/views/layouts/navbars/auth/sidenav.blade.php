ed000882275c7de817abb7e751c79db30d5d0f6d
Guilherme Andrade <guiandradeemail@gmail.com>
2024-12-14 23:01:59 -0300

    feat: change colors

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -3,10 +3,9 @@
     <div class="sidenav-header">
         {{-- <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i> --}}
-        <img src="/img/favicon.png" alt="" class="cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none">
         <a class="navbar-brand m-0" href="{{ route('buffet.dashboard', ['buffet'=>$buffet->slug]) }}"
             target="_blank">
-            <img src="{{ asset('img/logo-ct-dark.png') }}" class="navbar-brand-img h-100" alt="main_logo">
+            <img src="{{ asset('img/identidade-visual/buffettech_logo_icone.png') }}" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold">{{ $buffet->trading_name }}</span>
         </a>
     </div>
